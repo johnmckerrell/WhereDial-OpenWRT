7a4913c0da4c9c403e74c4edb2a7f74f788d75de
Elliot Adderton <e.adderton@liv.ac.uk>
2013-07-15 14:36:36 +0100

    Long Polling

--- bin/wheredial.sh
+++ bin/wheredial.sh
@@ -1,16 +1,18 @@
 #!/bin/ash
 
-# Init the Arduino
-#stty -F /dev/ttyACM0 cs8 9600 -ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke -noflsh -ixon -crtscts
-#stty -F /dev/ttyACM0
-
 # Get the users MAC address
 wheredial_mac=$(ifconfig | grep -m 1 -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}') 
 
-# Touch a log file
+# Log file
 log_file="/tmp/wheredial_log"
-touch "$log_file"
 echo "[$(uptime | awk '{print $1}')] - 0. Loading the wheredial ($wheredial_mac)" >> "$log_file"
+
+
+# Downloading Config
+wheredial_config=$(wget -qO- "http://mapme.at/api/wheredial.cs?config=yes&mac=$wheredial_mac")
+wheredial_url=$(echo "$wheredial_config" | awk -F, '{print $1":"$2$3}')
+wheredial_domain=$(echo "$wheredial_config" | awk -F, '{print $1}') 
+wheredial_sleep=$(echo "$wheredial_config" | awk -F, '{print $4}')
 
 while true; do
 
@@ -18,7 +20,7 @@ while true; do
 	echo 12345 > /dev/ttyACM0
 
 	# DNS
-	wheredial_dns_ping=$(ping mapme.at -c 1)
+	wheredial_dns_ping=$(ping "$wheredial_domain" -c 1)
 	wheredial_dns_status=$(echo "$wheredial_dns_ping" | grep 'received' | awk -F',' '{ print $2}' | awk '{ print $1}')
 	if [ "$wheredial_dns_status" != "1" ]; then
 		echo "[$(uptime | awk '{print $1}')] - 1. DNS Error" >> "$log_file"
@@ -31,13 +33,16 @@ while true; do
 		wheredial_dns_ip=$(echo "$wheredial_dns_ping" | sed -n 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
 		echo "[$(uptime | awk '{print $1}')] - 1. DNS Success, IP: $wheredial_dns_ip" >> "$log_file" 
 		echo 5 > /dev/ttyACM0
+		sleep 1
 	fi
 
 	# Get the page source 
-	wheredial_page_source=$(wget -S -qO- "http://mapme.at/api/wheredial.csv?mac=$wheredial_mac" 2>&1)
+	#wheredial_page_source=$(wget -S -qO- "http://mapme.at/api/wheredial.csv?mac=$wheredial_mac" 2>&1)
+	wheredial_page_source=$(/root/simplehttpc "$wheredial_mac" "$wheredial_position" "$wheredial_hash" )
 	if [ -n "$wheredial_page_source" ]; then
 		echo "[$(uptime | awk '{print $1}')] - 2. Connected to server" >> "$log_file" 
 		echo 4 > /dev/ttyACM0
+		sleep 1
 	else 
 		echo "[$(uptime | awk '{print $1}')] - 2. Couldn't connect to server" >> "$log_file" 	
 		echo 0 > /dev/ttyACM0                                                                                       
@@ -49,11 +54,12 @@ while true; do
 
 
 	# Status Code
-	wheredial_status_code=$(echo "$wheredial_page_source"| grep "HTTP/" | awk '{print $2}')
+	wheredial_status_code=$(echo "$wheredial_page_source"| grep "HTTP/1.1" | awk '{print $2}')
 
 	if [ $wheredial_status_code -ge 200 ] &&  [ $wheredial_status_code -le 300 ]; then 
 		echo "[$(uptime | awk '{print $1}')] - 3. Valid status code: $wheredial_status_code" >> "$log_file"
  		echo 3 > /dev/ttyACM0
+		sleep 1
 	else
 		echo "[$(uptime | awk '{print $1}')] - 3. Invalid status code: $wheredial_status_code" >> "$log_file" 
 		echo 0 > /dev/ttyACM0
@@ -67,10 +73,10 @@ while true; do
 	wheredial_page_content=$(echo "$wheredial_page_source" | tail -1)
 
 	# How much should I rotate?
-	wheredial_position=$(echo $wheredial_page_content | sed -e "s/\(.*\),\(.*\)$/\1/")
+	wheredial_position=$(echo $wheredial_page_content | sed -n 's/^\([0-9]\{1,3\}\),\([[:alnum:]]\{40\}\)\(,[[:alnum:]]*\)*$/\1/p')
 		
 	# What is the hash for my last request?
-	wheredial_hash=$(echo $wheredial_page_content | sed -e "s/\(.*\),\(.*\)$/\2/")
+	wheredial_hash=$(echo $wheredial_page_content | sed -n 's/^\([0-9]\{1,3\}\),\([[:alnum:]]\{40\}\)\(,[[:alnum:]]*\)*$/\2/p')
 
 	# Check we have the page content
 	if [ -n $wheredial_position ] && [ -n $wheredial_hash ]; then
@@ -96,7 +102,7 @@ while true; do
 		continue
 	fi
 	# Sleep for a moment. Hmm 60 times an hour, 1440 times a day :0
- 	sleep 60
+ 	sleep $wheredial_sleep
 	
 	echo "=============================================" >> "$log_file" 
 done
